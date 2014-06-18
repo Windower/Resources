17dfd6d66c5cc8d89240d7376d298d710813e9c3
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2014-06-18 11:51:01 +0200

    June update for XML resources, added regions, added search term to zones

---
+++ lua/regions.lua
@@ -0,0 +1,70 @@
+-- Automatically generated file: Regions
+
+return {
+    [0] = {id=0,en="San d'Oria",ja="サンドリア王国",de="San d'Oria",fr="San d'Oria"},
+    [1] = {id=1,en="Bastok",ja="バストゥーク共和国",de="Bastok",fr="Bastok"},
+    [2] = {id=2,en="Windurst",ja="ウィンダス連邦",de="Windurst",fr="Windurst"},
+    [3] = {id=3,en="Jeuno",ja="ジュノ大公国",de="Jeuno",fr="Jeuno"},
+    [4] = {id=4,en="Ronfaure",ja="ロンフォール",de="Ronfaure",fr="Ronfaure"},
+    [5] = {id=5,en="Zulkheim",ja="ザルクヘイム",de="Zulkheim",fr="Zulkheim"},
+    [6] = {id=6,en="Norvallen",ja="ノルバレン",de="Norvallen",fr="Norvallen"},
+    [7] = {id=7,en="Gustaberg",ja="グスタベルグ",de="Gustaberg",fr="Gustaberg"},
+    [8] = {id=8,en="Derfland",ja="デルフラント",de="Derfland",fr="Derfland"},
+    [9] = {id=9,en="Sarutabaruta",ja="サルタバルタ",de="Sarutabaruta",fr="Sarutabaruta"},
+    [10] = {id=10,en="Kolshushu",ja="コルシュシュ",de="Kolshushu",fr="Kolshushu"},
+    [11] = {id=11,en="Aragoneu",ja="アラゴーニュ",de="Aragoneu",fr="Aragoneu"},
+    [12] = {id=12,en="Fauregandi",ja="フォルガンディ",de="Fauregandi",fr="Fauregandi"},
+    [13] = {id=13,en="Valdeaunia",ja="バルドニア",de="Valdeaunia",fr="Valdeaunia"},
+    [14] = {id=14,en="Qufim",ja="クフィム",de="Qufim",fr="Qufim"},
+    [15] = {id=15,en="Li'Telor",ja="リ・テロア",de="Li'Telor",fr="Li'Telor"},
+    [16] = {id=16,en="Kuzotz",ja="クゾッツ",de="Kuzotz",fr="Kuzotz"},
+    [17] = {id=17,en="Vollbow",ja="ヴォルボー",de="Vollbow",fr="Vollbow"},
+    [18] = {id=18,en="Elshimo Lowlands",ja="低地エルシモ",de="Elshimo-Ebene",fr="Basses-Terres d'Elshimo"},
+    [19] = {id=19,en="Elshimo Uplands",ja="高地エルシモ",de="Elshimo-Hochland",fr="Hautes-Terres d'Elshimo"},
+    [20] = {id=20,en="Tu'Lia",ja="トゥー・リア",de="Tu'Lia",fr="Tu'Lia"},
+    [21] = {id=21,en="Dynamis",ja="デュナミス",de="Dynamis",fr="Dynamis"},
+    [22] = {id=22,en="Movalpolos",ja="ムバルポロス",de="Movalpolos",fr="Movalpolos"},
+    [23] = {id=23,en="Tavnazian Marquisate",ja="タブナジア侯国",de="Tavnazische Marquisate",fr="Marquisat de Tavnazia"},
+    [24] = {id=24,en="Tavnazian Archipelago",ja="タブナジア群島",de="Tavnazisches Archipel",fr="Archipel de Tavnazia"},
+    [25] = {id=25,en="Promyvion",ja="プロミヴォン",de="Promyvion",fr="Promyvion"},
+    [26] = {id=26,en="Lumoria",ja="ルモリア",de="Lumoria",fr="Lumoria"},
+    [27] = {id=27,en="Limbus",ja="リンバス",de="Limbus",fr="Limbus"},
+    [28] = {id=28,en="Empire of Aht Urhgan",ja="アトルガン皇国",de="Imperium von Aht Urhgan",fr="Empire d'Aht Urhgan"},
+    [29] = {id=29,en="West Aht Urhgan",ja="西アトルガン地方",de="West-Aht Urhgan",fr="Aht Urhgan occidental"},
+    [30] = {id=30,en="Mamool Ja Savagelands",ja="マムージャ蕃国",de="Wildnis der Mamool Ja",fr="Fief des Sauvages Mamool Ja"},
+    [31] = {id=31,en="Halvung Territory",ja="ハルブーン傭兵団領",de="Halvung-Territorien",fr="Territoire Halvung"},
+    [32] = {id=32,en="Arrapago Islands",ja="アラパゴ諸島",de="Arrapago-Inseln",fr="Iles d'Arrapago"},
+    [33] = {id=33,en="Ruins of Alzadaal",ja="アルザダール文明遺跡群",de="Ruinen von Alzadaal",fr="Ruines d'Alzadaal"},
+    [34] = {id=34,en="The Ronfaure Front",ja="ロンフォール戦線",de="Ronfaure-Front",fr="Front de Ronfaure"},
+    [35] = {id=35,en="The Norvallen Front",ja="ノルバレン戦線",de="Norvallen-Front",fr="Front de Norvallen"},
+    [36] = {id=36,en="The Gustaberg Front",ja="グスタベルグ戦線",de="Gustaberg-Front",fr="Front de Gustaberg"},
+    [37] = {id=37,en="The Derfland Front",ja="デルフラント戦線",de="Derfland-Front",fr="Front de Derfland"},
+    [38] = {id=38,en="The Sarutabaruta Front",ja="サルタバルタ戦線",de="Sarutabaruta-Front",fr="Front de Sarutabaruta"},
+    [39] = {id=39,en="The Aragoneu Front",ja="アラゴーニュ戦線",de="Aragoneu-Front",fr="Front d'Aragoneu"},
+    [40] = {id=40,en="The Fauregandi Front",ja="フォルガンディ戦線",de="Fauregandi-Front",fr="Front de Fauregandi"},
+    [41] = {id=41,en="The Valdeaunia Front",ja="バルドニア戦線",de="Valdeaunia-Front",fr="Front de Valdeaunia"},
+    [42] = {id=42,en="Orcish Hosts",ja="オーク帝国軍",de="Orc-Imperium",fr="Armée de l'empire orque"},
+    [43] = {id=43,en="Quadav Shieldwarriors",ja="クゥダフ兵団",de="Quadav-Armeekorps",fr="Guerriers quadavs"},
+    [44] = {id=44,en="Yagudo Theomilitary",ja="ヤグード教団軍",de="Yagudo-Theomilitär",fr="Théomilitaires yagudos"},
+    [45] = {id=45,en="Dark Kindred",ja="闇の王親衛隊",de="Schwarze Garde",fr="Milice du Seigneur des ombres"},
+    [46] = {id=46,en="Abyssea",ja="アビセア",de="Abyssea",fr="Abyssée"},
+    [47] = {id=47,en="The Threshold",ja="スレッショルド",de="Grenzland",fr="Firmament"},
+    [48] = {id=48,en="Abdhaljs",ja="アブダルス",de="Abdhaljs",fr="Abdhaljs"},
+    [49] = {id=49,en="The Sacred City of Adoulin",ja="神聖アドゥリン都市同盟",de="Heiliger Adouliner Städtebund",fr="Sainte Cité d'Adoulin"},
+    [50] = {id=50,en="The Adoulin Islands",ja="アドゥリン諸島",de="Adouliner Inseln",fr="Archipel d'Adoulin"},
+    [51] = {id=51,en="East Ulbuka Territory",ja="東ウルブカ地方",de="Ost-Ulbuka",fr="Ulbuka oriental"},
+    [52] = {id=52,en="Ra'Kaznar",ja="ラ・カザナル",de="Ra'Kaznar",fr="Ra'Kaznar"},
+}, {"id", "en", "ja", "de", "fr"}
+
+--[[
+Copyright © 2013-2014, Windower
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+    * Neither the name of Windower nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Windower BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+]]
