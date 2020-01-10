789889246c5818c749a7d51991c445548bee90f0
Chiaia <chiaiaxi@gmail.com>
2020-01-10 01:08:05 -0500

    Jan 9th, 2020 Update/2020 Copyright Update

--- resources_data/job_points.lua
+++ resources_data/job_points.lua
@@ -224,7 +224,7 @@ return {
 }, {"id", "en", "ja", "endesc", "jadesc"}
 
 --[[
-Copyright © 2013-2019, Windower
+Copyright © 2013-2020, Windower
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
