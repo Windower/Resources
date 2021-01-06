430b6450b9fb4be671b37c705bea6fe1d1729f89
WindowerBot <39810929+WindowerBot@users.noreply.github.com>
2021-01-06 00:47:52 +0000

    Deploy resources from commit 1212929a22d1138f9fafcd4571ce6610eb2ea457

--- resources_data/monster_abilities.lua
+++ resources_data/monster_abilities.lua
@@ -3519,7 +3519,7 @@ return {
 }, {"id", "en", "ja", "element", "icon_id", "monster_level", "prefix", "range", "targets", "tp_cost"}
 
 --[[
-Copyright © 2013-2020, Windower
+Copyright © 2013-2021, Windower
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
