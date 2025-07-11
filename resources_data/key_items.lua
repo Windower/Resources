fc06c863f6913d5aa1e9ca78cf616d74944617ee
WindowerBot <39810929+WindowerBot@users.noreply.github.com>
2025-07-11 01:31:24 +0000

    Deploy resources from commit 91b0deaaf36e5ce26c7e930ad80e446544bb1b46

--- resources_data/key_items.lua
+++ resources_data/key_items.lua
@@ -2984,6 +2984,7 @@ return {
     [3105] = {id=3105,en="♪Noble Chocobo companion",ja="♪ノーブルチョコボ",category="Mounts"},
     [3106] = {id=3106,en="♪Ixion companion",ja="♪イクシオン",category="Mounts"},
     [3107] = {id=3107,en="♪Phuabo companion",ja="♪フワボ",category="Mounts"},
+    [3108] = {id=3108,en="♪Craklaw companion",ja="♪クラックロー",category="Mounts"},
     [3136] = {id=3136,en="sheet of Shadow Lord tunes",ja="オーケストリオン譜：闇の王",category="Permanent Key Items"},
     [3137] = {id=3137,en="mystical canteen",ja="チカラを閉じ込めた飯盒",category="Temporary Key Items"},
     [3138] = {id=3138,en="Ygnas's insignia",ja="ユグナスの紹介状",category="Permanent Key Items"},
